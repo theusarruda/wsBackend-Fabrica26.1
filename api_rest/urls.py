b2b273fca6ff66d7f36366e01f245d07e477ca5c
Matheus Arruda <117926576+theusarruda@users.noreply.github.com>
2026-03-21 23:08:09 -0300

    :sparkles: Feature: Implementação da API e Templates

---
+++ api_rest/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import pokemon_list_create, atualizar_pokemon
+
+urlpatterns = [
+    path('', views.buscar),
+    path('', views.listar, name='listar'),
+    path('salvar/', views.salvar, name='salvar'),
+    path('deletar/<int:id>/', views.deletar),
+    path('pokemons/', pokemon_list_create),
+    path('pokemons/<int:id>/', atualizar_pokemon),
+]
--- api_rest/views.py
+++ api_rest/views.py
@@ -1,12 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .services import buscar_pokemon
+from .models import Pokemon
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import PokemonSerializer
 
 def buscar(request):
-    pokemon = None
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        data = buscar_pokemon(nome)
 
-    if 'nome' in request.GET:
-        nome = request.GET['nome']
-        pokemon = buscar_pokemon(nome)
+        return render(request, 'api_rest/buscar.html', {'pokemon': data})
 
-    return render(request, 'buscar.html', {'pokemon': pokemon})
+    return render(request, 'api_rest/buscar.html')
+
+def salvar(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        data = buscar_pokemon(nome)
+
+        if data:
+            Pokemon.objects.create(**data)
+
+        return redirect('listar')
+
+    return render(request, 'api_rest/salvar.html')
+
+def listar(request):
+    pokemons = Pokemon.objects.all()
+    return render(request, 'api_rest/listar.html', {'pokemons': pokemons})
+
+def deletar(request, id):
+    Pokemon.objects.get(id=id).delete()
+
+
+
+@api_view(["GET", "POST"])
+def pokemon_list_create(request):
+
+    if request.method == "GET":
+        pokemons = Pokemon.objects.all()
+        serializer = PokemonSerializer(pokemons, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        nome = request.data.get("nome")
+
+        data = buscar_pokemon(nome)
+
+        if not data:
+            return Response(
+                {"erro": "Pokemon não encontrado"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = PokemonSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(["PUT"])
+def atualizar_pokemon(request, id):
+    try:
+        pokemon = Pokemon.objects.get(id=id)
+    except Pokemon.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PokemonSerializer(pokemon, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 # Create your views here.
